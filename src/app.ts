0505518588bb5d5666e8fe6152fe16b602537cde
sohseyedi <sohseyedi@gmail.com>
2025-08-07 18:44:12 +0330

    import allRoutes and add path alias

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { connectDB } from './config/db';
+import { connectDB } from '@/config/db';
+import allRoutes from '@/core/routes/router';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: process.env.ALLOW_CORS_ORIGIN }));
 
+app.use('/api/v1', allRoutes);
 connectDB();
 
 app.listen(process.env.PORT, () => {
