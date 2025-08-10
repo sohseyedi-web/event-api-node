d1442ccd5006c2f15affb048bb26b6ab1b7c5ce5
sohseyedi <sohseyedi@gmail.com>
2025-08-10 22:24:18 +0330

    create authorize for check user role

---
+++ src/core/middleware/permission.guard.ts
@@ -0,0 +1,28 @@
+import { UserModel } from '@/modules/user/models/user';
+import { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
+import { IUser } from '../types';
+
+export default function authorize(...allowedRoles: string[]) {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.user?._id) {
+        throw createHttpError.Unauthorized('کاربر احراز هویت نشده است');
+      }
+
+      const user: IUser | null = await UserModel.findById(req.user._id);
+
+      if (!user) {
+        throw createHttpError.NotFound('کاربر پیدا نشد');
+      }
+
+      if (allowedRoles.length === 0 || allowedRoles.includes(user.role)) {
+        return next();
+      }
+
+      throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید');
+    } catch (error) {
+      next(error);
+    }
+  };
+}
