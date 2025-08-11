37bf8deb8b683cfee3652c96ab635bcf402ba6c9
sohseyedi <sohseyedi@gmail.com>
2025-08-11 17:37:15 +0330

    add transaction  routes

--- src/core/routes/router.ts
+++ src/core/routes/router.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userAuthRoutes from '@/modules/user/routes/userAuth';
 import eventRoutes from '@/modules/events/routes/event';
 import notificationRouter from '@/modules/notification/routes/notification';
+import transactionRouter from '@/modules/transaction/routes/transaction';
 
 const router = express.Router();
 
 router.use('/user', userAuthRoutes);
 router.use('/owner', eventRoutes);
 router.use('/notification', notificationRouter);
+router.use('/t', transactionRouter);
 
 export default router;
