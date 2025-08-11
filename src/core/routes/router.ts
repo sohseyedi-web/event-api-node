cb37aea67a7954eb434bd104c9b93843f04a652f
sohseyedi <sohseyedi@gmail.com>
2025-08-11 18:52:41 +0330

    add admin routes

--- src/core/routes/router.ts
+++ src/core/routes/router.ts
@@ -3,6 +3,7 @@ import userAuthRoutes from '@/modules/user/routes/userAuth';
 import eventRoutes from '@/modules/events/routes/event';
 import notificationRouter from '@/modules/notification/routes/notification';
 import transactionRouter from '@/modules/transaction/routes/transaction';
+import adminRouter from '@/modules/admin/routes/admin.routes';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/user', userAuthRoutes);
 router.use('/owner', eventRoutes);
 router.use('/notification', notificationRouter);
 router.use('/t', transactionRouter);
+router.use('/admin', adminRouter);
 
 export default router;
