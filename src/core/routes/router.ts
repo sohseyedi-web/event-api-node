cbc74e8e6372da6adc0efd242400c1afab207885
sohseyedi <sohseyedi@gmail.com>
2025-08-11 10:25:26 +0330

    add owner routes

--- src/core/routes/router.ts
+++ src/core/routes/router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import userAuthRoutes from '@/modules/user/routes/userAuth';
+import eventRoutes from '@/modules/events/routes/event';
 
 const router = express.Router();
 
 router.use('/user', userAuthRoutes);
+router.use('/owner', eventRoutes);
 
 export default router;
