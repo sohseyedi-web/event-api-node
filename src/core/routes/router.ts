294b0a3ad7f76a27e1858c4c708bcc2b679f1d62
sohseyedi <sohseyedi@gmail.com>
2025-08-11 14:03:38 +0330

    add notification routes

--- src/core/routes/router.ts
+++ src/core/routes/router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userAuthRoutes from '@/modules/user/routes/userAuth';
 import eventRoutes from '@/modules/events/routes/event';
+import notificationRouter from '@/modules/notification/routes/notification';
 
 const router = express.Router();
 
 router.use('/user', userAuthRoutes);
 router.use('/owner', eventRoutes);
+router.use('/notification', notificationRouter);
 
 export default router;
