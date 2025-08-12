f5662c3f9a3d4ac047c9ef51fb2eec23afb65f6e
sohseyedi <sohseyedi@gmail.com>
2025-08-12 12:22:36 +0330

    add middleware checking admin routes

--- src/modules/admin/routes/admin.routes.ts
+++ src/modules/admin/routes/admin.routes.ts
@@ -1,9 +1,14 @@
+import { verifyAccessToken } from '@/core/middleware/user.middleware';
 import eventAdminRoutes from './event';
 import notifAdminRoutes from './notification';
 import userAdminRoutes from './user';
+import { Router } from 'express';
+import authorize from '@/core/middleware/permission.guard';
+import { ROLES } from '@/config/constants';
 
-const router = require('express').Router();
+const router = Router();
 
+router.use(verifyAccessToken, authorize(ROLES.ROLES.ADMIN));
 router.use('/user', userAdminRoutes);
 router.use('/event', eventAdminRoutes);
 router.use('/notification', notifAdminRoutes);
