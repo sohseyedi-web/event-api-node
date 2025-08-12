dc4f2a8470402c167cdbf18bf7ff66fa1a808dc2
sohseyedi <sohseyedi@gmail.com>
2025-08-12 12:21:55 +0330

    change route user in admin for get detail with email

--- src/modules/admin/routes/user.ts
+++ src/modules/admin/routes/user.ts
@@ -2,7 +2,7 @@ import expressAsyncHandler from 'express-async-handler';
 import express from 'express';
 import {
   getAllUsers,
-  getUserDetailWithId,
+  getUserDetailWithEmail,
   userProfile,
   verifyUser,
 } from '../controllers/user.controller';
@@ -11,6 +11,6 @@ const router = express.Router();
 router.get('/list', expressAsyncHandler(getAllUsers as any));
 router.patch('/verify/:userId', expressAsyncHandler(verifyUser as any));
 router.get('/profile/:userId', expressAsyncHandler(userProfile as any));
-router.get('/detail/:phone', expressAsyncHandler(getUserDetailWithId as any));
+router.get('/profile', expressAsyncHandler(getUserDetailWithEmail as any));
 
 export default router;
