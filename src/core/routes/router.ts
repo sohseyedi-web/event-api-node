8fdf7556d68db2e355076e76047d0bf1db7fc0d2
sohseyedi <sohseyedi@gmail.com>
2025-08-12 12:19:54 +0330

    add public routes

--- src/core/routes/router.ts
+++ src/core/routes/router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userAuthRoutes from '@/modules/user/routes/userAuth';
+import publicRoutes from '@/modules/user/routes/event';
 import eventRoutes from '@/modules/events/routes/event';
 import notificationRouter from '@/modules/notification/routes/notification';
 import transactionRouter from '@/modules/transaction/routes/transaction';
@@ -8,9 +9,10 @@ import adminRouter from '@/modules/admin/routes/admin.routes';
 const router = express.Router();
 
 router.use('/user', userAuthRoutes);
+router.use('/p', publicRoutes); // public events
 router.use('/owner', eventRoutes);
 router.use('/notification', notificationRouter);
-router.use('/t', transactionRouter);
+router.use('/t', transactionRouter); // tranactions
 router.use('/admin', adminRouter);
 
 export default router;
