92b8c7074100a503d3e13173bd439cf7f9e494d2
sohseyedi <sohseyedi@gmail.com>
2025-08-13 22:30:46 +0330

    add socket.io integration for ticket handling

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { connectDB } from '@/config/db';
 import allRoutes from '@/core/routes/router';
+import http from 'http';
+import { Server } from 'socket.io';
+import { ticketSocketHandler } from '@/modules/tickets/socket/socket';
 
 dotenv.config();
 
@@ -11,10 +14,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_PARSER_SECRET_KEY));
 app.use(cors({ credentials: true, origin: process.env.ALLOW_CORS_ORIGIN }));
-
 app.use('/api/v1', allRoutes);
-connectDB();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: process.env.ALLOW_CORS_ORIGIN, credentials: true },
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Started on port ${process.env.PORT}`);
+ticketSocketHandler(io);
+
+connectDB().then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log(`Server Started on port ${process.env.PORT}`);
+  });
 });
